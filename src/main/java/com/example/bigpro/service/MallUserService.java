9cdd1048627a49a04eb6a03e9c78172da7e75425
sixgodhls <1543239669@qq.com>
2023-07-12 12:29:29 +0800

    添加服务

--- src/main/java/com/example/bigpro/service/MallUserService.java
+++ src/main/java/com/example/bigpro/service/MallUserService.java
@@ -1,5 +1,11 @@
 package com.example.bigpro.service;
 
+import com.example.bigpro.param.MallUserUpdateParam;
+
 public interface MallUserService {
     String login(String loginName,String passwordMD5);
+
+    Boolean updateUserInfo(MallUserUpdateParam mallUser,Long userId);
+
+    Boolean logout(Long userId);
 }
