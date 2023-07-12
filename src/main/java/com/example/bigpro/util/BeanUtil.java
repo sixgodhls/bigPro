b95d93d455018cdc054b02a948122a1c236edcb7
sixgodhls <1543239669@qq.com>
2023-07-12 12:30:07 +0800

    工具类

---
+++ src/main/java/com/example/bigpro/util/BeanUtil.java
@@ -0,0 +1,22 @@
+package com.example.bigpro.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.PropertyAccessorFactory;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.util.*;
+
+public abstract class BeanUtil {
+
+    public static Object copyProperties(Object source, Object target, String... ignoreProperties) {
+        if (source == null) {
+            return target;
+        }
+        BeanUtils.copyProperties(source, target, ignoreProperties);
+        return target;
+    }
+
+}
