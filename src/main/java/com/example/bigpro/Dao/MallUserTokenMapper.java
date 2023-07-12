af358315baa9ed177f2b358af9f8aa68666d7356
sixgodhls <1543239669@qq.com>
2023-07-12 12:27:08 +0800

    添加新操作

--- src/main/java/com/example/bigpro/Dao/MallUserTokenMapper.java
+++ src/main/java/com/example/bigpro/Dao/MallUserTokenMapper.java
@@ -1,12 +1,24 @@
 package com.example.bigpro.Dao;
 
+import com.example.bigpro.entity.MallUser;
 import com.example.bigpro.entity.MallUserToken;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MallUserTokenMapper {
+    int deleteByPrimaryKey(Long userId);
+
+    int insert(MallUserToken record);
+
     int insertSelective(MallUserToken record);
+
     MallUserToken selectByPrimaryKey(Long userId);
+
     MallUserToken selectByToken(String token);
+
     int updateByPrimaryKeySelective(MallUserToken record);
+
+    int updateByPrimaryKey(MallUserToken record);
+
+    int deleteByPrimaryKey(MallUser record);
 }
