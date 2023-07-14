69cf109213d480e349b194161b374e70c5c6e8ec
sixgodhls <1543239669@qq.com>
2023-07-14 12:43:36 +0800

    常用参数类

---
+++ src/main/java/com/example/bigpro/common/Constants.java
@@ -0,0 +1,10 @@
+package com.example.bigpro.common;
+
+public class Constants {
+    public final static  String FILE_UPLOAD_DIC="C:\\Users\\LENOVO\\Desktop\\spring\\bigPro\\src\\main\\resources\\images\\upload\\";
+    public final static int TOKEN_LENGTH = 32;//token字段长度
+    public final static int INDEX_CAROUSEL_NUM = 4;//轮播图数量
+    public final static int INDEX_HOTGOODS_NUM = 5;//轮播图数量
+    public final static int INDEX_NEWGOODS_NUM = 6;//轮播图数量
+    public final static int INDEX_GOODS_RECOMMOND_NUM = 6;//轮播图数量
+}
