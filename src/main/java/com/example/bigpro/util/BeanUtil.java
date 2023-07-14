441d65593a3530e7a55a861ae80d3b6d6a1e063c
sixgodhls <1543239669@qq.com>
2023-07-14 12:49:01 +0800

    工具类

--- src/main/java/com/example/bigpro/util/BeanUtil.java
+++ src/main/java/com/example/bigpro/util/BeanUtil.java
@@ -18,5 +18,32 @@ public abstract class BeanUtil {
         BeanUtils.copyProperties(source, target, ignoreProperties);
         return target;
     }
+    public static <T> List<T> copyList(List sources, Class<T> clazz) {
+        return copyList(sources, clazz, null);
+    }
 
+    public static <T> List<T> copyList(List sources, Class<T> clazz, Callback<T> callback) {
+        List<T> targetList = new ArrayList<>();
+        if (sources != null) {
+            try {
+                for (Object source : sources) {
+                    T target = clazz.newInstance();
+                    copyProperties(source, target);
+                    if (callback != null) {
+                        callback.set(source, target);
+                    }
+                    targetList.add(target);
+                }
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return targetList;
+    }
+
+    public static interface Callback<T> {
+        void set(Object source, T target);
+    }
 }
