f30237bf39bd9ae0e2290834d1fe2f797de87672
sixgodhls <1543239669@qq.com>
2023-07-17 13:46:00 +0800

    查找商品映射层

--- src/main/java/com/example/bigpro/Dao/MallGoodsMapper.java
+++ src/main/java/com/example/bigpro/Dao/MallGoodsMapper.java
@@ -1,6 +1,7 @@
 package com.example.bigpro.Dao;
 
 import com.example.bigpro.entity.MallGoods;
+import com.example.bigpro.util.PageQueryUtil;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -23,5 +24,58 @@ public interface MallGoodsMapper {
             + "</script>")
     List<MallGoods> findByPrimaryKeys(List<Long> goodsIds);
 
-
+    @Select("<script>"
+            + "SELECT "
+            + "* "
+            + "FROM tb_newbee_mall_goods_info "
+            + "<where> "
+            + "<if test=\"keyword != null and keyword != ''\"> "
+            + "AND (goods_name LIKE CONCAT('%', #{keyword}, '%') OR goods_intro LIKE CONCAT('%', #{keyword}, '%')) "
+            + "</if> "
+            + "<if test=\"goodsCategoryId != null and goodsCategoryId != ''\"> "
+            + "AND goods_category_id = #{goodsCategoryId} "
+            + "</if>"
+            + "<if test=\"goodsSellStatus != null\">"
+            + "AND goods_sell_status = #{goodsSellStatus}"
+            + "</if>"
+            + "</where>"
+            + "<if test=\"orderBy != null and orderBy != ''\">"
+            + "<choose>"
+            + "<when test=\"orderBy == 'new'\">"
+            + "ORDER BY goods_id DESC"
+            + "</when>"
+            + "<when test=\"orderBy == 'price'\">"
+            + "ORDER BY selling_price ASC"
+            + "</when>"
+            + "<otherwise>"
+            + "ORDER BY stock_num DESC"
+            + "</otherwise>"
+            + "</choose>"
+            + "</if>"
+            + "<if test=\"start != null and limit != null\">"
+            + "LIMIT #{start}, #{limit}"
+            + "</if>"
+            + "</script>")
+    List<MallGoods> findMallGoodsListBySearch(PageQueryUtil pageUtil);
+    @Select("<script>"
+            + "SELECT count(*) "
+            + "FROM tb_newbee_mall_goods_info "
+            + "<where>"
+            + "<if test=\"goodsName != null and goodsName != ''\">"
+            + "AND goods_name LIKE CONCAT('%', #{goodsName}, '%')"
+            + "</if>"
+            + "<if test=\"goodsSellStatus != null and goodsSellStatus != ''\">"
+            + "AND goods_sell_status = #{goodsSellStatus}"
+            + "</if>"
+            + "<if test=\"startTime != null and startTime.trim() != ''\">"
+            + "AND create_time &gt; #{startTime}"
+            + "</if>"
+            + "<if test=\"endTime != null and endTime.trim() != ''\">"
+            + "AND create_time &lt; #{endTime}"
+            + "</if>"
+            + "</where>"
+            + "</script>")
+    int getTotalMallGoodsBySearch(PageQueryUtil pageUtil);
+    @Select("select * from tb_newbee_mall_goods_info where goods_id =#{goodsId}")
+    MallGoods selectByPrimaryKey(Long goodsId);
 }
